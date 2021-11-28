984007eafc85221695de3bcdd909b0bc1c7ed3be
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2021-11-28 15:06:23 +0900

    janome適用

--- vect2.py
+++ vect2.py
@@ -1,8 +1,30 @@
 # vect2.py title_to_vector
 
+import numpy as np
 import pandas as pd
+from janome.tokenizer import Tokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def wakachi(text):
+    t = Tokenizer()
+    tokens = t.tokenize(text)
+    docs=[]
+    for token in tokens:
+        docs.append(token.surface)
+    return docs
+
+def vecs_array(documents):
+    docs = np.array(documents)
+    vectorizer = TfidfVectorizer(analyzer=wakachi,binary=True,use_idf=False)
+    vecs = vectorizer.fit_transform(docs)
+    return vecs.toarray()
+
 
 df_title = pd.read_excel('NovelComics.xlsx', index_col=0, usecols=['Title'])
 
+vector = [0] * 70
 for i in range(60):
-    print(df_title.iloc[i])
+    vector = vecs_array(wakachi(df_title.index.values[i]))
+
+
