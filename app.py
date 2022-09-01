655a3b512ea1746d3dc124fb0c8756ec5ffd8973
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-09-02 05:08:27 +0900

    csvにベクトル保存

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
 import os
 import math
 from flask import (
-    Flask, 
-    request, 
-    redirect, 
+    Flask,
+    request,
+    redirect,
     render_template)
 from vect import to_vector
 from scipy.spatial.distance import cosine
+import pandas as pd
+import numpy as np
 
 UPLOAD_FOLDER_ENTER = './static/images/image_enter'
 IMAGE_FOLDER_REFERENCE = './static/images/image_reference'
 
 entry_dict = {} # アップロードする画像 key=path, value=ベクトル
 reference_dict = {} # 比較する画像 key=path, value=ベクトル
-num_image_dict = { 'ONEPIECE.jpg':490000000,'ゴルゴ13.jpg':300000000, 
-'ドラゴンボール.jpg':260000000, 'NARUTO.jpg':250000000, '名探偵コナン.jpg':250000000,
-'鬼滅の刃.jpg':150000000,'BLEACH.jpg':120000000, 'ドラえもん.jpg':100000000,
-'ジョジョの奇妙な冒険.jpg':100000000,'進撃の巨人.jpg':100000000,
-'はじめの一歩.jpg':96000000,'バキ.jpg':85000000, 
-'キングダム.jpg':83000000, 'キャプテン翼.jpg':80000000, 'キン肉マン.jpg':75000000,
-'FAIRYTAIL フェアリーテイル.jpg':72000000
-} # 部数と画像ファイルのパス
+num_image_dict = {}
 
-def start_up(): # アプリ起動前から完成(実行)しておきたい所...
-    reference_dict.clear()
-    for references_file in os.listdir(IMAGE_FOLDER_REFERENCE):
-        reference_img_path = os.path.join(IMAGE_FOLDER_REFERENCE, references_file)
-        to_vector(references_file, reference_dict, reference_img_path)
+df = pd.read_csv('./static/csv/enter_book.csv')
+df_dict = df.to_dict(orient='dict')
+title = df_dict['title']
+num = df_dict['circulation']
+
+for i in range(len(num)):
+    num_image_dict[title[i]] = num[i]
+
+df_ref = pd.read_csv('./static/csv/reference_book.csv')
+for i in range(len(df_ref)-1):
+    reference_dict[df.loc[i,:].values[0]] = df.loc[i,:].values[1:].tolist()
 
 
 def get_similarity(entry_vector, reference_vector):
@@ -42,7 +43,6 @@ def get_similarity(entry_vector, reference_vector):
     """
     return 1 - cosine(entry_vector, reference_vector)
 
-start_up()
 
 app = Flask(__name__)
 
@@ -73,23 +73,23 @@ def uploads_file():
                 most_similar_img = ''
                 max_similarity = 0
                 for r_path, r_vector in reference_dict.items():
-                    if get_similarity(entry_dict[uploads_file], r_vector) > max_similarity:
-                        max_similarity = get_similarity(entry_dict[uploads_file], r_vector)
+                    tmp = np.array(r_vector)
+                    if get_similarity(entry_dict[uploads_file], tmp) > max_similarity:
+                        max_similarity = get_similarity(entry_dict[uploads_file], tmp)
                         most_similar_img = r_path
                         num_circ = num_image_dict[most_similar_img]
                 filename = most_similar_img.split('//')[-1] #windowsは//, macは\\
                 result = num_circ * max_similarity
                 return render_template(
-                'result.html',
-                filename=filename,
-                score=math.floor(max_similarity*100),
-                num_circ=num_circ,
-                result=math.floor(result)
+                    'result.html',
+                    filename=filename,
+                    score=math.floor(max_similarity*100),
+                    num_circ=num_circ,
+                    result=math.floor(result)
                 )
-                return redirect('/')
 
 
 
 #スクリプトからAPIを叩けるようにします。
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=8000)
--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,24 @@
 // 画像のプレビュー表示
 function previewImage(obj)
 {
-    var fileReader = new FileReader();
+    let fileReader = new FileReader()
     fileReader.onload = (function() {
-        document.getElementById('preview').src = fileReader.result;
-    });
-    fileReader.readAsDataURL(obj.files[0]);
+        document.getElementById('preview').src = fileReader.result
+    })
+    fileReader.readAsDataURL(obj.files[0])
 }
 
 // ボタンで表示変更
 function changeDisplay() {
-    var obj = document.getElementById('history');
-    state = obj.style.display;
-    console.log(state);
+    let obj = document.getElementById('history')
+    state = obj.style.display
+    console.log(state)
     if( state=="none" ){
-        obj.setAttribute("style","display:inline");
-        console.log("inline");
+        obj.setAttribute("style","display:inline")
+        console.log("inline")
     }
     else {
-        obj.setAttribute("style","display:none");
-        console.log("none");
+        obj.setAttribute("style","display:none")
+        console.log("none")
      }
  }
